c1d1e21081b23d0e28ae4f92bc91e403cf71e2c1
Vision70s <145845660+Vision70s@users.noreply.github.com>
2024-11-20 10:29:32 +0300

    Update renderer_1.ts

--- frontend/src/renderer_1.ts
+++ frontend/src/renderer_1.ts
@@ -5,26 +5,18 @@ import * as THREE from "three";
 
 export const renderer = (url: string) => {
     // НАЧАЛО БЕЗОПАСНОЙ ЗОНЫ
-    // Создаем новую сцену Three.js
-    const threeScene = new THREE.Scene();
-    // Добавляем освещение в сцену
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    
-    threeScene.add(ambientLight);
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(5, 10, 7.5);
-    threeScene.add(directionalLight);
-    
-    // Создаем инстанс Viewer с параметрами камеры
-    
+
     const viewer = new GaussianSplats3D.Viewer({
-    threeScene,
-    cameraUp: [0, -1, 0],
-    initialCameraPosition: [-2, -2, 4],
-    initialCameraLookAt: [0, 1.65, 0]
-    });
-    
-    // Загружаем внешний файл и добавляем его в Viewer
+    'cameraUp': [0, -5, 0],
+    'initialCameraPosition': [1, 0, -3],
+    'inMemoryCompressionLevel': 1,
+    'renderMode': GaussianSplats3D.RenderMode.OnChange,
+    'sceneRevealMode': GaussianSplats3D.SceneRevealMode.Gradual,
+    'splatSortDistanceMapPrecision': 32,
+    'sceneFadeInRateMultiplier': 20,
+    'initialCameraLookAt': [0, 1, 0]
+});
+   // Загружаем внешний файл и добавляем его в Viewer
     viewer
         .addSplatScene(
             url /* из места вызова функции прокидывается ссылка из БД */,
@@ -37,15 +29,16 @@ export const renderer = (url: string) => {
             animate();
         });
 
-    // Анимация сцены
-    function animate() {
-        requestAnimationFrame(() => setTimeout(animate, 1000 / 60)); // Ограничение FPS до 60
+.then(() => {
+    viewer.start();
 
-        viewer.update();
-        viewer.render();
-    }
+    // Запускаем анимацию перехода от точек к сплатам
+    viewer.animatePointCloudToSplats();
+})
+.catch(error => {
+    console.error("Ошибка при загрузке сцены:", error);
+});
 
-    animate();
 
     // КОНЕЦ БЕЗОПАСНОЙ ЗОНЫ
 };
