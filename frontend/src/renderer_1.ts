2da91e8c6eb63ea569b0d967d227697b0c4dfd68
Vision70s <145845660+Vision70s@users.noreply.github.com>
2024-11-20 10:21:21 +0300

    Update renderer_1.ts

--- frontend/src/renderer_1.ts
+++ frontend/src/renderer_1.ts
@@ -5,34 +5,25 @@ import * as THREE from "three";
 
 export const renderer = (url: string) => {
     // НАЧАЛО БЕЗОПАСНОЙ ЗОНЫ
-
-    // Создаем основную сцену для 3D объектов
+    // Создаем новую сцену Three.js
     const threeScene = new THREE.Scene();
-
-    // Инициализация Viewer
+    // Добавляем освещение в сцену
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    
+    threeScene.add(ambientLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(5, 10, 7.5);
+    threeScene.add(directionalLight);
+    
+    // Создаем инстанс Viewer с параметрами камеры
+    
     const viewer = new GaussianSplats3D.Viewer({
-        sharedMemoryForWorkers: false, // этот параметр лучше не трогать, упадет отображенеи фреймов через вставку на другие страницы
-        sceneFadeInRateMultiplier: 10,
-        threeScene: threeScene,
-        renderer: renderer3D,
-        renderMode: GaussianSplats3D.RenderMode.OnChange,
-
-        camera: new THREE.PerspectiveCamera(
-            65,
-            renderWidth / renderHeight,
-            0.1,
-            500,
-        ),
-        //useBuiltInControls: false,  // Не используем встроенные контролы
+    threeScene,
+    cameraUp: [0, -1, 0],
+    initialCameraPosition: [-2, -2, 4],
+    initialCameraLookAt: [0, 1.65, 0]
     });
-
-    const camera = viewer.camera;
-    camera.position.set(-1, -4, 6);
-    camera.up.set(0, -1, 0).normalize();
-    camera.lookAt(new THREE.Vector3(0, 4, 0));
-
-
-
+    
     // Загружаем внешний файл и добавляем его в Viewer
     viewer
         .addSplatScene(
