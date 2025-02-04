b24d22a37d2f34c2b928156630e87aeaac8bb536
amantulo <a.mantulo@sloboda-studio.com>
2025-02-04 14:26:53 +0300

    changing camera position

--- temp/src/project4/index.js
+++ temp/src/project4/index.js
@@ -3,7 +3,7 @@ import * as GaussianSplats3D from "../gaussian-splats-3d.module.js"
 
 const viewer = new GaussianSplats3D.Viewer({
     'cameraUp': [0, -1, 0],
-    'initialCameraPosition': [1.97906, 0.48267, -6.47377],
+    'initialCameraPosition': [ 2.042969746283587, 4.7470375733382544, 2.9667169128794133],
     'inMemoryCompressionLevel': 1,
     'renderMode': GaussianSplats3D.RenderMode.OnChange,
     'sceneRevealMode': GaussianSplats3D.SceneRevealMode.Gradual,
@@ -11,7 +11,7 @@ const viewer = new GaussianSplats3D.Viewer({
     'sceneFadeInRateMultiplier': 20,
     'dynamicScene': false,
     // 'initialCameraLookAt': [	1.03942, 3.57931, -4.03766],
-    'initialCameraLookAt': [  0.39246, 2.65146, -0.42320],
+    'initialCameraLookAt': [  -2.39246, 2.65146, -0.82320],
     'sharedMemoryForWorkers': false
 });
 
@@ -26,11 +26,16 @@ viewer.addSplatScene('../assets/Breig_final.ksplat', {
 .then(() => {
   viewer.start();
 
+  setTimeout(() => {
+    console.log("Camera Position:", viewer.camera.position);
+    console.log("Camera Target:", viewer.camera.target);
+}, 5000); 
+
   // Access the camera controls (if available)
   const controls = viewer.controls; // Hypothetical property to access controls
   if (controls) {
     controls.minDistance = 3; // Set minimum zoom distance
-    controls.maxDistance = 9; // Set maximum zoom distance
+    controls.maxDistance = 6; // Set maximum zoom distance
 
     // Optionally, limit vertical camera angles
     controls.minPolarAngle = Math.PI / 4; // Minimum angle (45 degrees above horizon)
